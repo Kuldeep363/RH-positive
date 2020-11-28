8a4a4204c9c2f9aded1bf9b3465c6275edee27d6
Rishabh Tanwar <54466428+Rishabhtanwar00@users.noreply.github.com>
2020-11-28 13:38:55 +0530

    Add files via upload

---
+++ rishab/navbar.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+import {BsDroplet , BsPeople } from 'react-icons/bs';
+
+import {BiHome} from 'react-icons/bi';
+
+import {FiAward} from 'react-icons/fi';
+
+import {IoCallOutline} from 'react-icons/io5';
+
+import '../navbar/navbar.styles.css';
+
+
+const Navbar = () =>{
+    return(
+       <div className="navbar">
+       <div className="options">
+          <Link className="option">
+             <div className="icon"><BiHome/></div>
+             <p>Home</p>
+          </Link> 
+          <Link className="option">
+             <div className="icon"><BsDroplet/></div>
+             <p>Donate blood</p>
+          </Link> 
+          <Link className="option">
+             <div className="icon"><FiAward/></div>
+             <p>Host camp</p>
+          </Link> 
+          <Link className="option">
+             <div className="icon"><BsPeople/></div>
+             <p>About us</p>
+          </Link> 
+          <Link className="option">
+             <div className="icon"><IoCallOutline/></div>
+             <p>Contact</p>
+          </Link> 
+       </div>
+       </div>
+    );
+}
+
+export default Navbar;
