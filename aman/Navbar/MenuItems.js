7c610bb280c98d7fcedb746c01369619d53ef660
Aman <55349238+X-genius@users.noreply.github.com>
2020-11-28 13:51:02 +0530

    Navbar : Navbar css and js, MenuItems, Button.css

---
+++ aman/Navbar/MenuItems.js
@@ -0,0 +1,43 @@
+export const MenuItems = [
+    {
+        title : 'Home',
+        url : '#',
+        cName : 'nav-links'
+    } ,
+    
+    {
+        title : 'Donate Blood',
+        url : '#',
+        cName : 'nav-links'
+    },
+
+    {
+        title : 'Host Camp',
+        url : '#',
+        cName : 'nav-links'
+    },
+
+    {
+        title : 'About us',
+        url : '#',
+        cName : 'nav-links'
+    },
+
+    {
+        title : 'Contact us',
+        url : '#',
+        cName : 'nav-links'
+    },
+
+    {
+        title : 'Register',
+        url : '#',
+        cName : 'nav-links-mobile'
+    },
+
+    {
+        title : 'Login',
+        url : '#',
+        cName : 'nav-links-mobile'
+    }
+]
