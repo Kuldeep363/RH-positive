1bd14200066fab9876ef2acf9e467fbef5c32688
Aman <55349238+X-genius@users.noreply.github.com>
2020-11-30 12:49:11 +0530

    updated Image Container with new image

--- aman/addImage/addImage.js
+++ aman/addImage/addImage.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './addCssImage.css'
-import donation from '../Images/donor.jpg'
+import donation from '../Images/home-3.png'
 
 export default function AddImages()
 {
@@ -8,9 +8,9 @@ export default function AddImages()
         <div className = "container">
         <img src = {donation} alt = "donor" className = "imageStyle"/>
         <div className = "text-block">
-              <h1 className = "bv">Become Volunteer</h1>
-              <p className = "paraSp">Volunteer is the bridge between the service and needed person. Come and join us in this beautiful course</p>
-              <button className="btnInbv"  id ="joinUs" type="submit" >Join us</button>
+              <h1 className = "becomeVolunteer">Become Volunteer</h1>
+              <p className = "paragraph">Volunteer is the bridge between the service and needed person. Come and join us in this beautiful course</p>
+              <button className="btnForJoin"  id ="joinUs" type="submit" >Join us</button>
         </div>
         </div>
     )
