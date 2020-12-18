69fba1a983e68cfce51ca0f7a648fd7975af214e
Aman <55349238+X-genius@users.noreply.github.com>
2020-12-18 13:44:26 +0530

    donor Pages update

---
+++ aman/AdditionalPages/donorPage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './donorPage.css'
+import donorGirl from '../Images/update-1.png'
+import CustomButton from '../CustomButton/CustomButton'
+
+export default function donorPage(){
+    return(
+        <div className = "containerEmailPage">
+        <div className = "containerButtons">
+            <CustomButton guideButton className = "searchBloodBtn">Search Blood</CustomButton>
+            <CustomButton guideButton className = "updateBloodBtn">Update Blood</CustomButton>
+            <CustomButton guideButton className = "addPostBtn">Add Post</CustomButton>
+            <CustomButton guideButton className = "sendEmailsBtn">send Emails</CustomButton>
+        </div>
+            <div className = "imgAndBtn">
+            <img src = {donorGirl} alt = "GirlDonorImage" className = "girlDonorImage"/>
+            <div className = "sideTwoBtns">
+            <button className = "Btn-1"><i className="fa fa-arrow-circle-o-right"></i>  Existing Donor</button>
+                <button className = "Btn-2">New Donor  <i className="fa fa-arrow-circle-o-right"></i></button>
+                
+            </div>
+            </div>
+        </div>
+    )}
